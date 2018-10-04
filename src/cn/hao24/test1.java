5bd24868e9ff9782a358c625a4f348445fa30a61
wangmin <wangm@hao24.cn>
2018-10-04 16:57:17 +0800

    first

---
+++ src/cn/hao24/test1.java
@@ -0,0 +1,15 @@
+/*
+ * 文件名：test1.java
+ * 版权：Copyright 好享购物股份有限公司;
+ * 描述：<描述>
+ * 修改人：Administrator
+ * 修改时间：2018年10月4日
+ * 跟踪单号: <跟踪单号>
+ * 修改单号: <修改单号>
+ * 修改内容：<修改内容>
+ */
+package cn.hao24;
+
+public class test1
+{
+}
---
+++ src/cn/hao24/test2.java
@@ -0,0 +1,20 @@
+/*
+ * 文件名：test2.java
+ * 版权：Copyright 好享购物股份有限公司;
+ * 描述：<描述>
+ * 修改人：Administrator
+ * 修改时间：2018年10月4日
+ * 跟踪单号: <跟踪单号>
+ * 修改单号: <修改单号>
+ * 修改内容：<修改内容>
+ */
+package cn.hao24;
+
+public class test2
+{
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("test1");
+    }
+}
